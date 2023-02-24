a848d5cc05738e9fb56268e34cd19b5e0a3d5e59
mesciuS <giovadev@gmail.com>
2023-02-24 17:06:04 +0100

    feat: struttura finale

--- js/script.js
+++ js/script.js
@@ -15,16 +15,24 @@ for (let i = 1; i <= 100; i++) {
     // creo IF per i multipli di 3
     if (i % 3 == 0) {
         newEl.innerHTML = "fizz";
+        newEl.style.backgroundColor = "#0cd6a1";
     }
 
+    else {
+        newEl.style.backgroundColor = "#1389b2";
+    }
+    
     // creo IF per multipli di 5
     if (i % 5 == 0) {
         newEl.innerHTML = "buzz";
+        newEl.style.backgroundColor = "#ffd166";
     }
 
     // creo IF per multipli di entrambi
     if (i % 3 == 0 && i % 5 == 0) {
         newEl.innerHTML = "fizzbuzz"
+        newEl.style.backgroundColor = "#f0466f";
     }
+
 } 
 
