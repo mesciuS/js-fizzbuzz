c9f4a46487e351763ef0aa18acd29e3cc59b0ceb
mesciuS <giovadev@gmail.com>
2023-02-24 16:30:50 +0100

    feat: struttura JS

--- js/script.js
+++ js/script.js
@@ -3,8 +3,28 @@
 let containerEl = document.getElementById("container");
 
 // creo la funzione con for
-for (let i = 0; i < 100; i++); {
+for (let i = 1; i <= 100; i++) {
 
-    console.log("ciao");
+    // creo un elemento html all'interno del container
+    let newEl = document.createElement("div");
+
+    newEl.innerHTML = i;
+
+    containerEl.append(newEl);
+
+    // creo IF per i multipli di 3
+    if (i % 3 == 0) {
+        newEl.innerHTML = "fizz";
+    }
+
+    // creo IF per multipli di 5
+    if (i % 5 == 0) {
+        newEl.innerHTML = "buzz";
+    }
+
+    // creo IF per multipli di entrambi
+    if (i % 3 == 0 && i % 5 == 0) {
+        newEl.innerHTML = "fizzbuzz"
+    }
 } 
 
