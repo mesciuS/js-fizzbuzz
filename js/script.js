c02139f511c5706060c5373572a3009ca01ca04f
mesciuS <giovadev@gmail.com>
2023-02-24 16:01:19 +0100

    feat: push hotwell

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+// creo una variabile da assegnare al genitore
+let containerEl = document.getElementById("container");
+
+// creo la funzione con for
+for (let i = 0; i < 100; i++); {
+
+    console.log("ciao");
+} 
+
